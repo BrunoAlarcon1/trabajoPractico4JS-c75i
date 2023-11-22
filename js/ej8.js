5b7f30acb5d3bf1814669f49d2efe00355a56b9a
bruno.alarcon <alarconbruno14@gmail.com>
2023-11-22 11:53:24 -0300

    agrego ejercicios(falta terminar 7, 9 y 10)

--- js/ej8.js
+++ js/ej8.js
@@ -1,21 +1,24 @@
-class Persona{
-    constructor(nombre,edad,profesion){
-        this.nombre= nombre;
-        this.edad=edad;
-        this.profesion=profesion;
-    }
-    saludar(){
-        return (document.write(`<p>Hola, soy ${this.nombre}, tengo ${this.edad} años y soy ${this.profesion}. Un gusto saludarte</p>`));
-    }
-    despedirse(){
-        return (document.write(`<p>Perdon tengo que irme, acabo de recordar que tengo una reunion agendada, un gusto saludarte. Nos vemos!</p>`));
-        
-    }
+class Persona {
+  constructor(nombre, edad, profesion) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.profesion = profesion;
+  }
+  saludar() {
+    return document.write(
+      `<p>Hola, soy ${this.nombre}, tengo ${this.edad} años y soy ${this.profesion}. Un gusto saludarte</p>`
+    );
+  }
+  despedirse() {
+    return document.write(
+      `<p>Perdon tengo que irme, acabo de recordar que tengo una reunion agendada, un gusto saludarte. Nos vemos!</p>`
+    );
+  }
 }
 
-const persona1 = new Persona("juan",20,"veterinario");
+const persona1 = new Persona("juan", 20, "veterinario");
 persona1.saludar();
 persona1.despedirse();
-const persona2 = new Persona("Ricardo",40,"Contador");
+const persona2 = new Persona("Ricardo", 40, "Contador");
 persona2.saludar();
 persona2.despedirse();
